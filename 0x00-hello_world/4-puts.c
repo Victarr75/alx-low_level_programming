37e91344fec71fbd9f5732505fa2c778b8da1b50
Victarr <korirvictor75@gmail.com>
2023-09-11 00:27:55 +0300

    new scrips

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* Print the message using puts */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
