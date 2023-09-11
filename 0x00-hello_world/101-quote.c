e0a53bd25acc9978fa985b60a9bbb346d0a3bf11
Victarr <korirvictor75@gmail.com>
2023-09-11 07:54:40 +0300

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -6,10 +7,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 
-		2015-10-19\n";
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, 59);
-	return (1);
+    write(STDERR_FILENO, message, 59);
+    return (1);
 }
-
