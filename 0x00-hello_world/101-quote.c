aadb63166e85bb7e4a533851fe13585e60205c6e
Victarr <korirvictor75@gmail.com>
2023-09-11 07:41:06 +0300

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
@@ -9,7 +8,7 @@ int main(void)
 {
 	const char *message =
         "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 59); /* 59 is the length of the message */
+	write(STDERR_FILENO, message, 59);
 	return (1);
 }
 
