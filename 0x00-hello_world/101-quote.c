f47e978fe9fc2dc378797f16c27737ef6c1b0a98
Victarr <korirvictor75@gmail.com>
2023-09-11 07:22:08 +0300

    script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 59); /* 59 is the length of the message */
+	return (1);
+}
+
