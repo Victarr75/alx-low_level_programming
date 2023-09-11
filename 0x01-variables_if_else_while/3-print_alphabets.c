cbb1ab5b8bfe55296b93e07511f5ed90d3c8a8b4
Victarr <korirvictor75@gmail.com>
2023-09-11 15:14:58 +0300

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: "lower and uppercase"
+ * return: always 0
+ */
+int main(void)
+{
+	char L;
+	char U;
+
+	for (L = 'a' ; L <= 'z' ; L++)
+	{
+		putchar(L);
+	}
+	for (U = 'A' ; U <= 'Z' ; U++)
+	{
+		putchar(U);
+	}
+	putchar('\n');
+	return (0);
+}
