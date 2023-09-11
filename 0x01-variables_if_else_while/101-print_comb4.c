97a027922f73f1e28e8de1a9c4ae1fe897f1cba9
Victarr <korirvictor75@gmail.com>
2023-09-11 20:12:39 +0300

    epic

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,17 +2,17 @@
 /**
  * main - entry point
  * Description: "three digit combo"
- * Result: Always 0
+ * Result: always 0
  */
 int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0 ; digit1 <=7 ; digit1++)
+	for (digit1 = 0 ; digit1 <= 7 ; digit1++)
 	{
-		for(digit2 = digit1 + 1 ; digit2 <= 8 ; digit2++)
+		for (digit2 = digit1 + 1 ; digit2 <= 8 ; digit2++)
 		{
-			for(digit3 = digit2 + 1 ; digit3 <= 9; digit3++)
+			for (digit3 = digit2 + 1 ; digit3 <= 9; digit3++)
 			{
 				putchar('0' + digit1);
 				putchar('0' + digit2);
