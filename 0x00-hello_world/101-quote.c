2d6ee3e0d8e1d7bbc22870e801a3a86faadbe2cb
Victarr <korirvictor75@gmail.com>
2023-09-11 07:38:15 +0300

    correct

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,9 @@
  */
 int main(void)
 {
-    const char *message =
+	const char *message =
         "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59); /* 59 is the length of the message */
-
-    return (1);
+	write(STDERR_FILENO, message, 59); /* 59 is the length of the message */
+	return (1);
 }
 
