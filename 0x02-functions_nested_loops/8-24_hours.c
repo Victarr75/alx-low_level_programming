9456f8697f6e7a26b662eaec622ded830c4666c4
Victarr <korirvictor75@gmail.com>
2023-09-13 13:18:44 +0300

    new changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,10 +6,27 @@
  */
 void jack_bauer(void)
 {
-	int i;
+	int w, x, y, z;
 	
-	for (i = 00:00 ; i <= 23:59 ; i++)
+	for (w = 0 ; w <= 2 ; w++)
 	{
-		_putchar(i);
+		for (x = 0 ; x <= 9 ; x++)
+		{
+		if (w <= 1 && x <= 9) || (w <= 2 && x <= 3)
+		{
+			for (y = 0 ; y <= 5 ; y+)
+			{
+				for (z = 0 ; z<= 9 ; z++)
+				{
+					_putchar(w + '0');
+					_putchar(x + '0');
+					_putchar(':');
+					_putchar(y + '0');
+					_putchar(z + '0');
+					_putchar('\n');
+				}
+		}
+		}
+		}
 	}
 }
