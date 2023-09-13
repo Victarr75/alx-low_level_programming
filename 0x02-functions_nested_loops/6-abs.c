99b189f524b591e357eed222cfa27583e74020db
Victarr <korirvictor75@gmail.com>
2023-09-13 10:45:20 +0300

    new changes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - 'Prints absolute value of a number'
+ *
+ * Return: Returns the absolute value.
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n > zero)
+	{
+		return (n*1);
+	}
+	else
+	{
+		return (n*-1);
+	}
+}
