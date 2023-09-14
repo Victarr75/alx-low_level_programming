552e2ca9d3f5ad43f0ed1dfc813cae940d6a66ed
Victarr <korirvictor75@gmail.com>
2023-09-14 19:23:36 +0300

    loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
