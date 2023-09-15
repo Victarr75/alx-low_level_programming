5192d6e39e7b2dfd51336e8fdc59ca33ba60090b
Victarr <korirvictor75@gmail.com>
2023-09-16 00:16:55 +0300

    loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,6 +9,7 @@
 void print_number(int n)
 {
 	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
@@ -16,7 +17,7 @@ void print_number(int n)
 	}
 	if (i / 10)
 	{
-		print_number(i /10);
+		print_number(i / 10);
 	}
 	_putchar(i % 10 + '0');
 }
