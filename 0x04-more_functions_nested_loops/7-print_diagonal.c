bd9824d07172501bbc531a7d09bf203265d4bf09
Victarr <korirvictor75@gmail.com>
2023-09-15 22:10:39 +0300

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,20 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+	for (i = 0 ; i < n ; i++)
 	{
-		int i, j;
-
-		for (i = 0 ; i < n ; i++)
+		for (j = 0 ; j < i ; j++)
 		{
-			for (j = 0 ; j < n ; j++)
-			{
-				if (j == 1)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+
+		_putchar('\n');
 	}
 }
