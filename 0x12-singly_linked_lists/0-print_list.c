4c1be03e10f6ce5b94b7ea0da7f2108b9c258c3e
Victarr <korirvictor75@gmail.com>
2023-10-20 01:40:24 +0300

    xfiles

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nth;
 
-	nelem = 0;
+	nth = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		nth++;
 	}
-	return (nelem);
+	return (nth);
 }
