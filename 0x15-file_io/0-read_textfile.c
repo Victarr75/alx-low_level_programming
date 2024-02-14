db1013448e76b9bb71eb4c7cbea90345b7f1d10e
Victarr75 <korirvictor75@gmail.com>
2024-02-14 14:34:49 +0300

    input

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,43 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define BUF_SIZE 1024
-
-/**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
- *
- * Return: The actual number of letters it could read and print.
- *         If the file cannot be opened or read, return 0.
- *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
- */
 ssize_t read_textfile(const char *filename, size_t letters) {
+    char *buffer;
+    ssize_t file_descriptor, bytes_read, bytes_written;
+
     if (filename == NULL)
-        return 0;
+        return (0);
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+        return (0);
 
-    char buffer[BUF_SIZE];
-    size_t total_read = 0;
-    ssize_t bytes_read;
-
-    while (total_read < letters && (bytes_read = read(fd, buffer, BUF_SIZE)) > 0) {
-        ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-        if (bytes_written != bytes_read) {
-            close(fd);
-            return 0;
-        }
-        total_read += bytes_written;
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL) {
+        close(file_descriptor);
+        return (0);
     }
 
-    close(fd);
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1) {
+        perror("read");
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
 
-    if (total_read < letters)
-        return total_read;
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1) {
+        perror("write");
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
 
-    return letters;
+    free(buffer);
+    close(file_descriptor);
+    return (bytes_written);
 }
